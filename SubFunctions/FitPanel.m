7163e5e683b8b0633ca3db2d374c0e4330ad7ee2
BC <boc5244@psu.edu>
2019-04-17 13:53:44 -0400

    Update FitPanel.m

--- SubFunctions/FitPanel.m
+++ SubFunctions/FitPanel.m
@@ -8,55 +8,62 @@ function [fitData] = FitPanel(X,t,t_new,varargin)
 %   OUTPUTS:
 %       fitData     :   fit data  
 %---------------------------------------------------------------------------------------------------------------------------------
-% Example Inputs %    
-% clear ; clc ; close all    
-% data = [];
-% t_new = 0:(1/1000):20;
-% load('E:\Experiment_HeadExcitation\Chirp\HeadFree\fly_2_trial_8_Amp_15.mat','data','t_p')
-% t = t_p;
-% X = panel2deg(data(:,2));  % pattern x-pos: subtract mean and convert to deg [deg]
-% clear data t_p
-%---------------------------------------------------------------------------------------------------------------------------------
-	Xw = X;
-%     Xw(Xw>180 & Xw<=360) = Xw(Xw>180 & Xw<=360) - 360; % wrapped
-    thresh = 3; % velcoity threshold to detect panel transitions 
-    n = length(Xw); % length of signal
-    IV = (1:n)'; % index vector
-    dX = [abs(diff(Xw)) ; 0]; % pattern velocity
-    mIdx = [IV(1) ; IV(dX>thresh) ; IV(end)]; % detect transition points
-    if any(mIdx==n) && any(mIdx==1)
-        mIdx = mIdx(2:end-1); % make sure last & 1st point are not detected, we will add it later
-    elseif any(mIdx==n)
-        mIdx = mIdx(1:end-1); % make sure last point is not detected, we will add it later
-	elseif any(mIdx==1)
-        mIdx = mIdx(2:end); % make sure first point is not detected, we will add it later 
-    end
-    mX = Xw(mIdx); % pattern position corresponding to transtion points
-	mt = t(mIdx); % pattern time corresponding to transtion points
-    m2Idx = [IV(1) ; mean([mIdx(1:end-1),mIdx(2:end)],2) ; IV(end)]; % peak indicies between transition points
-    m2X = Xw(round(m2Idx)); % peak pattern positions between transition points
-  	m2t = t(round(m2Idx)); % peak times between transition points
+Xw = X;
+thresh = 3; % velcoity threshold to detect panel transitions 
+n = length(Xw); % length of signal
+IV = (1:n)'; % index vector
+dX = [abs(diff(Xw)) ; 0]; % pattern velocity
+mIdx = [IV(1) ; IV(dX>thresh) ; IV(end)]; % detect transition points
+if any(mIdx==n) && any(mIdx==1)
+    mIdx = mIdx(2:end-1); % make sure last & 1st point are not detected, we will add it later
+elseif any(mIdx==n)
+    mIdx = mIdx(1:end-1); % make sure last point is not detected, we will add it later
+elseif any(mIdx==1)
+    mIdx = mIdx(2:end); % make sure first point is not detected, we will add it later 
+end
+mX = Xw(mIdx); % pattern position corresponding to transtion points
+mt = t(mIdx); % pattern time corresponding to transtion points
+m2Idx = [IV(1) ; mean([mIdx(1:end-1),mIdx(2:end)],2) ; IV(end)]; % peak indicies between transition points
+m2X = Xw(round(m2Idx)); % peak pattern positions between transition points
+m2t = t(round(m2Idx)); % peak times between transition points
 
-    [xData, yData] = prepareCurveData( m2t, m2X ); % prepare data for fit
+% dX = diff(m2X);
+% dX_mean = mean(dX);
+% dX_std = std(dX);
+% dX_err_idx = abs(dX)>(dX_mean+1*dX_std);
+% dX_err_time = m2t(dX_err_idx);
+% dX_err = m2X(dX_err_idx);
+% corr = find(dX_err_idx==1);
+% for kk = 1:length(corr)
+%     rng = 360 - m2X(corr(kk));
+%     if rng>=360/2
+%         m2X(corr(kk)) = min(m2X);
+%     else
+%         m2X(corr(kk)) = max(m2X);
+%     end
+% end
+
+[xData, yData] = prepareCurveData( m2t, m2X ); % prepare data for fit
 %     ft = 'splineinterp'; % set up fittype and options
-    ft = 'linearinterp';
+ft = 'linearinterp';
 
-    [fitPattern, ~] = fit( xData, yData, ft, 'Normalize', 'on' ); % fit model to data
+[fitPattern, ~] = fit( xData, yData, ft, 'Normalize', 'on' ); % fit model to data
 
-    fitData = fitPattern(t_new); % apply fit to new time vector
-    
-    % debug
-    if nargin==4
-        if varargin{1}==true
-            figure (1) ; clf ; hold on
-                plot(t,Xw,'k')
+fitData = fitPattern(t_new); % apply fit to new time vector
+
+% debug
+if nargin==4
+    if varargin{1}==true
+        figure (1) ; clf ; hold on
+            plot(t,Xw,'k')
 %                 plot(mt,mX,'g*')
-                plot(m2t,m2X,'r*')    
-                plot(t_new,fitData,'b')
-                xlim([0 t(end)])
-                xlabel('Time (s)')
-                ylabel('Pattern (deg)')
-                legend('Panel','Peak Marker','Fit')
-        end
+            plot(m2t,m2X,'r*')
+%             plot(dX_err_time,dX_err,'om')
+            plot(t_new,fitData,'b')
+            xlim([0 t(end)])
+            xlabel('Time (s)')
+            ylabel('Pattern (deg)')
+            legend('Panel','Peak Marker','Fit')
     end
+end
 end
