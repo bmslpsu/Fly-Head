b9bcb2bfca8a31a4b88b15bc521fbd4cb49146ac
BC <boc5244@psu.edu>
2019-04-17 16:24:19 -0400

    Correction for wrapping

--- SubFunctions/FitPanel.m
+++ SubFunctions/FitPanel.m
@@ -27,22 +27,24 @@ m2Idx = [IV(1) ; mean([mIdx(1:end-1),mIdx(2:end)],2) ; IV(end)]; % peak indicies
 m2X = Xw(round(m2Idx)); % peak pattern positions between transition points
 m2t = t(round(m2Idx)); % peak times between transition points
 
-% dX = diff(m2X);
-% dX_mean = mean(dX);
-% dX_std = std(dX);
-% dX_err_idx = abs(dX)>(dX_mean+1*dX_std);
-% dX_err_time = m2t(dX_err_idx);
-% dX_err = m2X(dX_err_idx);
-% corr = find(dX_err_idx==1);
-% for kk = 1:length(corr)
-%     rng = 360 - m2X(corr(kk));
-%     if rng>=360/2
-%         m2X(corr(kk)) = min(m2X);
-%     else
-%         m2X(corr(kk)) = max(m2X);
-%     end
-% end
+% Detect transition points not to fit & fix them
+dX = diff(m2X);
+dX_mean = mean(dX);
+dX_std = std(dX);
+dX_err_idx = abs(dX)>(dX_mean+2*dX_std) | circshift(abs(dX)>(dX_mean+1*dX_std),1);
+dX_err_time = m2t(dX_err_idx);
+dX_err = m2X(dX_err_idx);
+err_idx = find(dX_err_idx==1);
+for kk = 1:length(err_idx)
+    rng = 360 - m2X(err_idx(kk));
+    if rng>=360/2
+        m2X(err_idx(kk)) = min(m2X);
+    else
+        m2X(err_idx(kk)) = max(m2X);
+    end
+end
 
+% Fit points
 [xData, yData] = prepareCurveData( m2t, m2X ); % prepare data for fit
 %     ft = 'splineinterp'; % set up fittype and options
 ft = 'linearinterp';
@@ -51,6 +53,27 @@ ft = 'linearinterp';
 
 fitData = fitPattern(t_new); % apply fit to new time vector
 
+% Detect transition points not from fit & fix them
+dX = diff(fitData);
+dX_mean = mean(dX);
+dX_std = std(dX);
+dX_err_idx = abs(dX)>(dX_mean+2*dX_std) | circshift(abs(dX)>(dX_mean+2*dX_std),1);
+dX_err_time = t_new(dX_err_idx);
+dX_err = fitData(dX_err_idx);
+err_idx = find(dX_err_idx==1);
+for kk = 1:length(err_idx)
+	idx = err_idx(kk);
+    for jj = kk:length(fitData)
+        if ~ismember(jj,err_idx(kk))
+            fitData(err_idx(kk)) = fitData(jj);
+        else
+            break
+        end
+    end
+end
+
+% fitData = rad2deg(wrapToPi(deg2rad(fitData)));
+
 % debug
 if nargin==4
     if varargin{1}==true
@@ -58,8 +81,8 @@ if nargin==4
             plot(t,Xw,'k')
 %                 plot(mt,mX,'g*')
             plot(m2t,m2X,'r*')
-%             plot(dX_err_time,dX_err,'om')
-            plot(t_new,fitData,'b')
+            plot(dX_err_time,dX_err,'og')
+            plot(t_new,fitData,'b-')
             xlim([0 t(end)])
             xlabel('Time (s)')
             ylabel('Pattern (deg)')
