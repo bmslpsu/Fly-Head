1eb8e1bd017c6a7a8b045b3c10dfa2f0f52764bf
Benjamin Cellini <31375763+boc5244@users.noreply.github.com>
2019-09-16 16:17:49 -0400

    Update MakeFig_SOS_HeadFree_Variance.m

--- Figure/obj/Final/MakeFig_SOS_HeadFree_Variance.m
+++ Figure/obj/Final/MakeFig_SOS_HeadFree_Variance.m
@@ -5,8 +5,8 @@ function [FIG] = MakeFig_SOS_HeadFree_Variance()
 %   OUTPUTS:
 %       FIG     :   figure handle
 %---------------------------------------------------------------------------------------------------------------------------------
-% root = 'H:\DATA\Rigid_Data\';
-root = 'Q:\';
+root = 'H:\DATA\Rigid_Data\';
+% root = 'Q:\';
 % Select files
 [FILE,~] = uigetfile({'*.mat', 'DAQ-files'}, ...
     'Select head angle trials', root, 'MultiSelect','off');
@@ -146,6 +146,7 @@ FIG.Units = 'inches';
 FIG.Position = 1.5*[1 1 4 4];
 movegui(FIG,'center')
 bins = 8;
+clear ax
 
 ax(1) = subplot(2,2,1); hold on ; title('Head STD') ; xlabel(['Gain (' char(176) '/' char(176) ')'])
 histogram(HEAD(:,:,1),bins,'FaceColor','b')
@@ -160,4 +161,31 @@ ax(4) = subplot(2,2,4); hold on ; xlabel(['Phase (' char(176) ')'])
 histogram(WING(:,:,2),bins,'FaceColor','r')
 
 set(ax,'FontSize',8,'YLim',[0 5],'Box','on')
+
+%% Head & Wings Variance Combined
+Head = nan(5,HeadFree.N.fly,2);
+Wing = nan(5,HeadFree.N.fly,2);
+for kk = 1:HeadFree.N.fly
+	Head(:,kk,1) = HeadFree.FLY{kk,headIdx}.STD{5}(:,xIdx);
+    Head(:,kk,2) = rad2deg(HeadFree.FLY{kk,headIdx}.CircSTD{6}(:,xIdx));
+  	Wing(:,kk,1) = HeadFree.FLY{kk,wingIdx}.STD{5}(:,xIdx);
+    Wing(:,kk,2) = rad2deg(HeadFree.FLY{kk,wingIdx}.CircSTD{6}(:,xIdx));
+end
+HEAD = mean(Head,1);
+WING = mean(Wing,1);
+
+FIG = figure (4); clf
+FIG.Color = 'w';
+FIG.Units = 'inches';
+FIG.Position = [1 1 4 2];
+movegui(FIG,'center')
+edges = 5:5:45;
+clear ax
+
+ax(1) = subplot(1,1,1); hold on ; title('Phase STD') ; xlabel(['Phase (' char(176) ')']) ; ylabel('Count')
+histogram(HEAD(:,:,2),edges,'FaceColor','b','FaceAlpha',1)
+histogram(WING(:,:,2),edges,'FaceColor','r','FaceAlpha',1)
+legend('Head','Wings','box','off')
+
+set(ax,'FontSize',8,'YLim',[0 10],'Box','on')
 end
