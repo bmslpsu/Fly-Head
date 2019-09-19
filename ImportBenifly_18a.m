9133d8dfa3a2e83ee95f81786e382cfbbd6110f4
amb7595 <47258939+amb7595@users.noreply.github.com>
2019-09-19 14:56:53 -0400

    Import for old matlab

---
+++ ImportBenifly_18a.m
@@ -0,0 +1,49 @@
+function filedata = ImportBenifly_18a(filename)
+%IMPORTFILE Import numeric data from a text file as a matrix.
+%   FLY7TRIAL4 = IMPORTFILE(FILENAME) Reads data from text file FILENAME
+%   for the default selection.
+%
+
+%% Initialize variables.
+delimiter = ',';
+startRow = 2;
+endRow = inf;
+
+%% Format for each line of text:
+%   column1: double (%f)
+%	column2: double (%f)
+%   column3: double (%f)
+%	column4: double (%f)
+%   column5: double (%f)
+%	column6: double (%f)
+% For more information, see the TEXTSCAN documentation.
+formatSpec = '%f%f%f%f%f%f%[^\n\r]';
+
+%% Open the text file.
+fileID = fopen(filename,'r');
+
+%% Read columns of data according to the format.
+% This call is based on the structure of the file used to generate this
+% code. If an error occurs for a different file, try regenerating the code
+% from the Import Tool.
+dataArray = textscan(fileID, formatSpec, endRow(1)-startRow(1)+1, 'Delimiter', delimiter, 'TextType', 'string', 'HeaderLines', startRow(1)-1, 'ReturnOnError', false, 'EndOfLine', '\r\n');
+for block=2:length(startRow)
+    frewind(fileID);
+    dataArrayBlock = textscan(fileID, formatSpec, endRow(block)-startRow(block)+1, 'Delimiter', delimiter, 'TextType', 'string', 'HeaderLines', startRow(block)-1, 'ReturnOnError', false, 'EndOfLine', '\r\n');
+    for col=1:length(dataArray)
+        dataArray{col} = [dataArray{col};dataArrayBlock{col}];
+    end
+end
+
+%% Close the text file.
+fclose(fileID);
+
+%% Post processing for unimportable data.
+% No unimportable data rules were applied during the import, so no post
+% processing code is included. To generate code which works for
+% unimportable data, select unimportable cells in a file and regenerate the
+% script.
+
+%% Create output variable
+filedata = table(dataArray{1:end-1}, 'VariableNames', {'Time','Head','LWing','RWing','Abdomen','WBF'});
+
