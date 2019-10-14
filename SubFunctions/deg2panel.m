1352c9fc8490598cc1905705ab65a491185870a9
boc5244 <31375763+boc5244@users.noreply.github.com>
2019-10-14 00:15:02 -0400

    Update deg2panel.m

--- SubFunctions/deg2panel.m
+++ SubFunctions/deg2panel.m
@@ -1,9 +1,12 @@
-function [panel] = deg2panel(deg)
+function [panel] = deg2panel(deg,res)
 %% MakePosFunction_Chirp: makes chirp position function
 %   INPUTS:
 %       deg     :   data in degrees
 %   OUTPUTS:
 %       panel   :   data in panel #
 %---------------------------------------------------------------------------------------------------------------------------------=
-panel = round(deg/3.75); % convert to steps [deg]
+if nargin==1
+    res = 3.75; % default
+end
+panel = round(deg/res); % convert to steps [deg]
 end
