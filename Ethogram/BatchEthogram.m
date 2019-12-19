312773c8466222e4b5317bd4aaa7a6215a09d490
amb7595 <47258939+amb7595@users.noreply.github.com>
2019-12-19 13:43:36 -0500

    Comments

--- Ethogram/BatchEthogram.m
+++ Ethogram/BatchEthogram.m
@@ -1,30 +1,38 @@
+% Batch Ethogram
+% Audrey Blizard
+% 12/19/2019
+% Loads in a series of .mat files containing fly videos. It then allows the
+% user to run the MakeEthogram function on each file and save the results.
+
 %% Select File(s) %%
 %---------------------------------------------------------------------------------------------------------------------------------
 clear;close all;clc
 
-root = 'E:\Walking_Experiments\SOS\mat';
+root = 'E:\Walking_Experiments\SOS\mat';                   % PC file path
+%root = '/Volumes/Data_Audrey/Walking_Experiments/SOS/mat';  % Mac file path
 
-[FILES, dirpath] = uigetfile('*.mat', 'Select fly trials', root, 'MultiSelect','on');
+[FILES, dirpath] = uigetfile('*.mat', 'Select Walking Videos', root, 'MultiSelect','on');
 FILES = cellstr(FILES)';
 
 nTrial = length(FILES); % total # of trials
-playback = 5;
+playback = 5;           % used in function, increment of frames shown
 
 %% Load video data & run tracking software %%
 %---------------------------------------------------------------------------------------------------------------------------------
 for jj = 1:nTrial
     % Load video data
-    load([dirpath FILES{jj}]); % load video data
+    load([dirpath FILES{jj}]);              % load video data
     disp('Load File: Done')
-    
-    etho=MakeEthogram(rawVid,playback);
-    
-    
+    disp(FILES{jj})
+    etho=MakeEthogram(rawVid,playback);     % calls MakeEthogram Function
 
     % Save data
     disp('Save Data...')
-    save(fullfile(root,'\ethograms', FILES{jj}),'etho');
+    save(fullfile(root,'\ethograms', FILES{jj}),'etho');   % PC save
+    %save(fullfile(root,'/ethograms', FILES{jj}),'etho');    % Mac save
     
     close all
     clc
 end
+
+disp('All Trials Completed')
--- Ethogram/MakeEthogram.m
+++ Ethogram/MakeEthogram.m
@@ -1,39 +1,44 @@
+
 function [etho]=MakeEthogram(rawVid,playback)
+% Allows user to manually track the flys state as walking or standing. 
+% Click in the figure margins. Left click when the fly starts walking, 
+% right click when the fly starts standing. Outputs a row vector with 
+% 1 (walking) and 0 (stamding) for each frame of the video. 
 
-global state
+global state                        % sets state as global to be used in subfunction
 
-hfig = figure(1); %gen figure handles
-set(hfig,'ButtonDownFcn',@clicker) %set button down function to clicker (see below)
-vidData = squeeze(rawVid);
-[~,~,nFrame] = size(vidData); % get dimensions  of video data
-hfig.UserData=1; %use userdata as a flag.
-etho=NaN(nFrame,1);
-state=NaN;
+hfig = figure(1); % figure handles
+set(hfig,'ButtonDownFcn',@clicker)  % mouse click calls clicker subfunction
+vidData = squeeze(rawVid);          % removes extraneous video dimension
+[~,~,nFrame] = size(vidData);       % get # of frames in video 
+hfig.UserData=1;                    % use userdata as a flag.
+etho=NaN(nFrame,1);                 % preallocates etho as NaN vector
+state=NaN;                          % state is initally NaN
 tic
-for n = playback:playback:nFrame
+for n = playback:playback:nFrame    % loops through frames in playback increments
    imshow(vidData(:,:,n));
-   etho((n-playback+1):n)=state;
+   etho((n-playback+1):n)=state;    % sets current value and previous to state
    pause(.01)
 end
 toc
 
-NaNidx = isnan(etho);
-definedstate = etho(~NaNidx);
-firststate = definedstate(1);
-etho(NaNidx) = firststate;
+nanIdx = isnan(etho);               % sets all initial values to first declared state
+stateIdx = etho(~nanIdx);
+etho(nanIdx)=stateIdx(1);
 
-imagesc(etho')
-pause()
 
-%what happens when you click on the figure
-function clicker(hobject,~)
-whichbutton=get(hobject, 'selectiontype'); %this gets what button was pressed
-switch whichbutton % if anything really but i put this here anyways
-    case 'normal' %left mouse click
-        state=1; %set the object's user data to 0 (ie 0 for live feed)
+imagesc(etho')                      % shows results of tracking
+pause(1)
+
+
+function clicker(hobject,~)         % called on button click
+whichbutton=get(hobject, 'selectiontype'); % which button was pressed
+switch whichbutton 
+    case 'normal'                   % left mouse click
+        state=1;                    % sets state to 1
         disp('walk')
-    case 'alt' %right mouse click
-        state=0;
+    case 'alt'                      % right mouse click
+        state=0;                    % sets state to 0
         disp('stand')
 end
 end
